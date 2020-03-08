dc7738d9415d5a1bbeeef5ff13b1da945951fc70
Tsunagu <bhgpp948@gmail.com>
2020-03-08 15:48:44 +0900

    Init

---
+++ LottieSample/LottieSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lottie.Forms;
+using Reactive.Bindings;
+
+namespace LottieSample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        public ReactiveProperty<bool> ButtonAnimIsPlaying { get; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<bool> ButtonIsVisible { get; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<bool> ParticleIsPlaying { get; } = new ReactiveProperty<bool>();
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            ButtonStartCommand = new DelegateCommand(ButtonStart);
+            ButtonAnimFinishCommand = new DelegateCommand(ButtonAnimFinish);
+
+            //lottie init
+            var animationView = new AnimationView()
+            {
+
+            };
+
+            ButtonIsVisible.Value = true;
+            ButtonAnimIsPlaying.Value = false;
+        }
+
+
+        public DelegateCommand ButtonStartCommand { get; set; }
+        private void ButtonStart()
+        {
+
+            ButtonAnimIsPlaying.Value = true;
+        }
+
+        public DelegateCommand ButtonAnimFinishCommand { get; set; }
+        private void ButtonAnimFinish()
+        {
+            ButtonIsVisible.Value = false;
+            ParticleIsPlaying.Value = true;
+
+        }
+    }
+}
---
+++ LottieSample/LottieSample/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Prism.Commands;
+using Prism.Mvvm;
+
+// Lottie用に追加
+using Lottie.Forms;
+
+namespace LottieSample.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
